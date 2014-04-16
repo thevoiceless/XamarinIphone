c05ec86d631c739d11ae8f03f0f823c10e7a3675
Riley Moses <victi.resurgo@gmail.com>
2014-04-16 14:57:14 -0600

    Initial commit

---
+++ Hello_World_iPhone/Hello_World_iPhoneViewController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+namespace Hello_World_iPhone
+{
+	public partial class Hello_World_iPhoneViewController : UIViewController
+	{
+		protected int numberOfTimesClicked = 0;
+
+		public Hello_World_iPhoneViewController(IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			// Releases the view if it doesn't have a superview.
+			base.DidReceiveMemoryWarning();
+			
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void actionButtonClick (NSObject sender)
+		{
+			this.lblOutput.Text = "Button \"" +  ((UIButton) sender).CurrentTitle + "\" clicked.";
+		}
+
+		#region View lifecycle
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			
+			// Perform any additional setup after loading the view, typically from a nib.
+			this.btnClickMe.TouchUpInside += (sender, eventArgs) => {
+				this.numberOfTimesClicked++;
+				this.lblOutput.Text = "Clicked " + this.numberOfTimesClicked.ToString() + " times";
+			};
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+		}
+
+		public override void ViewDidAppear(bool animated)
+		{
+			base.ViewDidAppear(animated);
+		}
+
+		public override void ViewWillDisappear(bool animated)
+		{
+			base.ViewWillDisappear(animated);
+		}
+
+		public override void ViewDidDisappear(bool animated)
+		{
+			base.ViewDidDisappear(animated);
+		}
+
+		#endregion
+	}
+}
+
